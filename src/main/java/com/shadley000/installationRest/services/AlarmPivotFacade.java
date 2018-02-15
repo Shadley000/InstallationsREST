a845c4599c6d3aa5e031612ad00ab327c4f746e5
stephen <stephenjhadley@gmail.com>
2018-02-15 07:08:57 -0700

    removed code to calculate custom pivot rows

--- src/main/java/com/shadley000/installationRest/services/AlarmPivotFacade.java
+++ src/main/java/com/shadley000/installationRest/services/AlarmPivotFacade.java
@@ -27,29 +27,29 @@ public class AlarmPivotFacade {
     private static String SQL_GET_ALARM_PIVOT = "SELECT ID_INSTALLATION, ID_ALARM_TYPE, ALARM_COUNT, ALARM_DATE "
             + " FROM ALARM_PIVOT WHERE ID_INSTALLATION = ? AND ALARM_DATE >= ? AND ALARM_DATE < ? AND ALARM_COUNT >0";
 
-    public  Map<Integer, int[]> getPivot(int installationId, Date fromDate, Date toDate) {
-        
-         Map<Integer, int[]> map = new HashMap<>();
-                 
+    public Map<Integer, int[]> getPivot(int installationId, Date fromDate, Date toDate) {
+
+        Map<Integer, int[]> map = new HashMap<>();
+
+        int length = convertDateToIndex(fromDate, toDate);
+
         try (Connection connection = SQLConnectionFactory.getConnection()) {
             PreparedStatement stmt = connection.prepareStatement(SQL_GET_ALARM_PIVOT);
             stmt.setInt(1, installationId);
             stmt.setString(2, DATE_FORMAT.format(fromDate));
             stmt.setString(3, DATE_FORMAT.format(toDate));
             ResultSet rs = stmt.executeQuery();
-            int length = convertDateToIndex(fromDate, toDate);
             while (rs.next()) {
                 Integer idAlarmType = rs.getInt("ID_ALARM_TYPE");
                 int alarmCount = rs.getInt("ALARM_COUNT");
-                Date alarmDate = rs.getDate( "ALARM_DATE" );
-                
+                Date alarmDate = rs.getDate("ALARM_DATE");
+
                 int[] histogram = map.get(idAlarmType);
                 if (histogram == null) {
                     histogram = initializeArray(length);
                     map.put(idAlarmType, histogram);
                 }
-                histogram[convertDateToIndex(fromDate,alarmDate)]= alarmCount;
-
+                histogram[convertDateToIndex(fromDate, alarmDate)] = alarmCount;
             }
         } catch (SQLException ex) {
             Logger.getLogger(InstallationFacade.class.getName()).log(Level.SEVERE, null, ex);
@@ -57,14 +57,16 @@ public class AlarmPivotFacade {
 
         return map;
     }
-    private int convertDateToIndex(Date fromDate, Date d)
-    {
-        return  (int)Math.round((d.getTime() - fromDate.getTime()) / (double) 86400000);
+
+    private int convertDateToIndex(Date fromDate, Date d) {
+        return (int) Math.round((d.getTime() - fromDate.getTime()) / (double) 86400000);
     }
-    private int[] initializeArray(int length)
-    {
+
+    private int[] initializeArray(int length) {
         int[] histogram = new int[length];
-        for(int i=0;i<length;i++) histogram[i]=0;
+        for (int i = 0; i < length; i++) {
+            histogram[i] = 0;
+        }
         return histogram;
     }
 }
